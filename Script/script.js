b21aefbe388c9a367f821d6f6928d2adfb217a19
YureshMajhi <yureshmee@gmail.com>
2022-11-23 15:18:37 +0545

    Huge Changes-Almost EVerything

--- Script/script.js
+++ Script/script.js
@@ -1,82 +1,101 @@
 let myLibrary = [];
 
-const nameInput = document.querySelector("#name-value");
-const authorInput = document.querySelector("#author-value");
-const statusInput = document.querySelector("#status-value");
-let arrayCount = 0;
-let contents = document.querySelector(".main");
+// initializing Value
+const form = document.querySelector("#form");
+const nameVal = document.querySelector("#name-el");
+const authorVal = document.querySelector("#author-el");
+const statusVal = document.querySelector("#status-el");
+const submitBtn = document.querySelector("#submit-el");
+const displayBook = document.querySelector(".main");
+const myLibraryStorage = JSON.parse(localStorage.getItem("myLibrary"));
 
-const arrFromLocal = JSON.parse(localStorage.getItem("myLibrary"));
-
-if (arrFromLocal) {
-  myLibrary = arrFromLocal;
-  contents.innerHTML = generateList(myLibrary);
-  arrayCount = JSON.parse(localStorage.getItem("arrayCount"));
+if (myLibraryStorage) {
+  myLibrary = myLibraryStorage;
+  generateList();
 }
 
-let form = document.querySelector(".form-container");
-form.style.display = "none";
-document.querySelector("#add").addEventListener("click", function () {
-  form.style.display = "block";
-});
-
-function Book(name, author, status) {
+// storing value using constructor
+function Book(name, author, id) {
   this.name = name;
   this.author = author;
-  this.status = status;
+  this.id = id;
 }
 
+// giving value to constructor
 function addBookToLibrary() {
-  let book = new Book(nameInput.value, authorInput.value, statusInput.value);
+  let book = new Book(
+    nameVal.value,
+    authorVal.value,
+    (id = new Date().getTime())
+  );
   return book;
 }
 
-document.querySelector("#submit").addEventListener("click", function () {
-  onSubmit();
+// form display
+form.style.display = "none";
+
+document.querySelector("#add-book").addEventListener("click", function () {
+  form.style.display = "flex";
 });
 
-document.addEventListener("keydown", function (e) {
+// submit button clicked
+submitBtn.addEventListener("click", function () {
+  submit();
+});
+form.addEventListener("keydown", function (e) {
   if (e.key == "Enter") {
-    onSubmit();
+    submit();
   }
 });
 
-function generateList(arg) {
-  let items = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    items += `
-    <div class="card">
-        <p>Name = ${arg[i].name}</p>
-        <p>Author = ${arg[i].author}</p>
-        <p>Status = ${arg[i].status}</p>
-    </div>
-    `;
+function submit() {
+  form.style.display = "none";
+  if (nameVal.value && authorVal.value) {
+    myLibrary.push(addBookToLibrary());
+
+    generateList();
+
+    nameVal.value = "";
+    authorVal.value = "";
   }
-  return items;
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 }
 
-document.querySelector("#delete").addEventListener("click", function () {
-  myLibrary = [];
-  contents.innerHTML = generateList(myLibrary);
-  arrayCount = 0;
-  localStorage.clear();
-});
+function generateList() {
+  // clearing main before rendering all
+  displayBook.innerHTML = "";
 
-function onSubmit() {
-  if (
-    nameInput.value != "" &&
-    authorInput.value != "" &&
-    statusInput.value != ""
-  ) {
-    myLibrary[arrayCount] = addBookToLibrary();
-    arrayCount++;
-    localStorage.setItem("arrayCount", JSON.stringify(arrayCount));
-    nameInput.value = "";
-    authorInput.value = "";
-    statusInput.value = "";
-  }
-  contents.innerHTML = generateList(myLibrary);
-  form.style.display = "none";
+  //   generating library
+  myLibrary.forEach(function (bookEl) {
+    let element = document.createElement("div");
+    let name = document.createElement("p");
+    let author = document.createElement("p");
+    name.textContent = bookEl.name;
+    author.textContent = "By:- " + bookEl.author;
 
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    // creating delete button
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.id = bookEl.id;
+
+    // adding book info to document
+    element.appendChild(name);
+    element.appendChild(author);
+    element.appendChild(deleteBtn);
+    displayBook.appendChild(element);
+
+    deleteBtn.addEventListener("click", deleteBook);
+  });
+}
+
+// deleting book
+function deleteBook(e) {
+  myLibrary = myLibrary.filter(function (bookEl) {
+    if (e.target.id == bookEl.id) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  generateList();
 }
